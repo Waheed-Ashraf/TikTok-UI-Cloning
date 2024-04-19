deee998fa015c0b83f95acb8a33c68b1c1e852f8
waheed_ashrf <washraf124@gmail.com>
2024-04-19 15:18:45 +0200

    reaction section done

--- lib/views/home_view/home_view.dart
+++ lib/views/home_view/home_view.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -18,7 +21,6 @@ class _HomeViewState extends State<HomeView> {
           scrollDirection: Axis.vertical,
           children: const [
             HomePosts(),
-            HomePosts2(),
           ],
         ),
         Padding(
@@ -73,27 +75,110 @@ class _HomePostsState extends State<HomePosts> {
         Container(
           color: Colors.deepPurpleAccent,
         ),
+        Align(
+          alignment: Alignment(1, 1),
+          child: Container(
+            color: Colors.blue,
+            height: MediaQuery.of(context).size.height * .55,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Stack(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: Container(
+                          width: 44,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(22)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(1.0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(22),
+                              child: Image.asset(
+                                'assets/images/profile.png',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 12.5,
+                        child: Container(
+                          width: 18,
+                          decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(12)),
+                          child: const Center(
+                            child: Icon(
+                              Icons.add,
+                              size: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  const PostButton(
+                    postIcon: Icon(FontAwesomeIcons.solidHeart),
+                    postReach: "1.2M",
+                  ),
+                  const PostButton(
+                    postIcon: Icon(FontAwesomeIcons.solidMessage),
+                    postReach: "1.2M",
+                  ),
+                  const PostButton(
+                    postIcon: Icon(FontAwesomeIcons.solidBookmark),
+                    postReach: "1.2M",
+                  ),
+                  const PostButton(
+                    postIcon: Icon(FontAwesomeIcons.share),
+                    postReach: "1.2M",
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        width: 44,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage("assets/images/cd.png"))),
+                        child: Padding(
+                          padding: const EdgeInsets.all(9),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(22),
+                            child: Image.asset(
+                              'assets/images/profile.png',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        )
       ],
     );
   }
 }
 
-class HomePosts2 extends StatefulWidget {
-  const HomePosts2({super.key});
+class PostButton extends StatelessWidget {
+  final Icon postIcon;
+  final String postReach;
+  const PostButton(
+      {super.key, required this.postIcon, required this.postReach});
 
-  @override
-  State<HomePosts2> createState() => _HomePosts2State();
-}
-
-class _HomePosts2State extends State<HomePosts2> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          color: Colors.deepOrangeAccent,
-        ),
-      ],
+    return Column(
+      children: [postIcon, Text(postReach)],
     );
   }
 }
