467a3b007673810dfafb4c9c2422537d0fe0ea89
waheed_ashrf <washraf124@gmail.com>
2024-04-19 05:13:50 +0200

    tab bar done

--- lib/views/home_view/home_view.dart
+++ lib/views/home_view/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -10,10 +11,47 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      children: const [
-        HomePosts(),
-      ],
+    return DefaultTabController(
+      length: 4,
+      child: Stack(children: [
+        PageView(
+          children: const [
+            HomePosts(),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 24),
+          child: Container(
+            height: 40,
+            alignment: const Alignment(0, 0),
+            child: const TabBar(
+                indicatorSize: TabBarIndicatorSize.tab,
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.grey,
+                labelPadding: EdgeInsets.symmetric(horizontal: 0, vertical: 8),
+                dividerColor: Colors.transparent,
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                indicatorColor: Colors.white,
+                indicatorPadding: EdgeInsets.symmetric(
+                  horizontal: 25,
+                ),
+                tabs: [
+                  Text(
+                    'Your City',
+                  ),
+                  Text(
+                    'Following',
+                  ),
+                  Text(
+                    'For You',
+                  ),
+                  Text(
+                    'LIVE',
+                  ),
+                ]),
+          ),
+        )
+      ]),
     );
   }
 }
